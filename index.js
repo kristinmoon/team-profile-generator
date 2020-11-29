7e5730d047473f3346b9b94af9b63adfab5ffcec
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-11-29 16:15:24 -0700

    page template

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const generatePage = require('./src/page-template.js');
+
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'teamName',
+      message: 'What is your team name?',
+    }
+  ]);
+};
 
 const promptEmployee = teamData => {
   // if there's no team array property, create one
@@ -13,61 +25,60 @@ const promptEmployee = teamData => {
 
   console.log(`
   ===================
-  Add a New Employee
+  Add an Employee
   ===================
   `);
 
-  return inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'employeeName',
-        message: "Please enter your employee's name: (Required)",
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please enter a name.');
-          }
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'employeeName',
+      message: "Please enter your employee's name: (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter a name.');
         }
-      },
-      {
-        type: 'input',
-        name: 'employeeID',
-        message: "Please enter your employee's ID: (Required)",
-        validate: idInput => {
-          if (idInput) {
-            return true;
-          } else {
-            console.log('Please enter an ID.');
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'employeeEmail',
-        message: "Please enter your employee's email address: (Required)",
-        validate: emailInput => {
-          if (emailInput) {
-            return true;
-          } else {
-            console.log('Please enter an email address.');
-          }
-        }
-      },
-      {
-        type: 'list',
-        name: 'employeeRole',
-        message: 'What role does this employee have within the company?',
-        choices: ['Manager', 'Engineer', 'Intern'],
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAddEmployee',
-        message: 'Would you like to enter another employee?',
-        default: false
       }
-    ])
+    },
+    {
+      type: 'input',
+      name: 'employeeID',
+      message: "Please enter your employee's ID: (Required)",
+      validate: idInput => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log('Please enter an ID.');
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'employeeEmail',
+      message: "Please enter your employee's email address: (Required)",
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log('Please enter an email address.');
+        }
+      }
+    },
+    {
+      type: 'list',
+      name: 'employeeRole',
+      message: 'What role does this employee have within the company?',
+      choices: ['Manager', 'Engineer', 'Intern'],
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddEmployee',
+      message: 'Would you like to enter another employee?',
+      default: false
+    }
+  ])
     .then(employeeData => {
       teamData.employees.push(employeeData);
       if (employeeData.confirmAddEmployee) {
@@ -78,4 +89,5 @@ const promptEmployee = teamData => {
     });
 };
 
-promptEmployee();
+promptUser()
+  .then(promptEmployee);
---
+++ src/page-template.js
@@ -0,0 +1,75 @@
+// create header
+const generateHeader = headerText => {
+  if (!headerText) {
+    return 'My Team';
+  }
+
+  return `
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">${header}</a>
+    </div>
+    `;
+};
+
+const generateEmployees = employeesArr => {
+  return `
+    <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+      ${employeesArr
+      .map(({ employeeName, employeeID, employeeEmail, employeeRole }) => {
+        return `
+          <span class="card-title">${employeeName}, ${employeeRole}</span>
+            <ul class="collection">
+              <li class="collection-item">ID: ${employeeID} $</li>
+              <li class="collection-item">Email: ${employeeEmail}</li>
+            </ul>
+          </div>
+        `;
+      })
+      .join('')}
+    </section>
+    `;
+};
+
+
+
+module.exports = templateData => {
+  // destructure employees and header data from templateData based on property key names
+  const { employees, ...header } = templateData;
+
+
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile Generator</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <nav>
+      ${generateHeader(header)}
+    </nav>
+    <main>
+      <div class="row">
+          <div class="col s12 m6">      
+          ${generateEmployees(employees)}
+      </div>
+      </div>
+    </main>
+    <footer class="page-footer">
+      <div class="footer-copyright">
+        <div class="container">
+        © 2020 Moon, Ink
+        </div>
+      </div>
+    </footer>
+  </body>
+  </html>
+  `;
+};
